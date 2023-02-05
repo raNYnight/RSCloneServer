96a227ec45a7cb4aad50146cda0ae63ac13880df
ranynigth <rany4ka@gmail.com>
2023-02-05 18:21:00 +0300

    feat: implement DB connect, add user controller

---
+++ index.js
@@ -0,0 +1,11 @@
+const exp = require("constants");
+const express = require("express");
+const userRouter = require("./routes/user_routes");
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.json());
+app.use("/", userRouter);
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
