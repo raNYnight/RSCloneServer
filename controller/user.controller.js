22f6b65dd22ae155439d099d744ab69057399fc1
AlmondChips <chips6t@gmail.com>
2023-02-07 01:35:53 +0300

    feat: add check for unique new user & add authorize function

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,25 +1,55 @@
 const db = require("../db");
-
+const { checkPassword } = require("../libs/bcrypt");
+const encrypt = require("../libs/bcrypt");
 class UserController {
   async getUsers(req, res) {
     const users = await db.query(`select *
     from users`);
     console.log(users.rows);
+    if (!req || !res) return users.rows;
     res.json(users.rows);
   }
+
   async getSingleUser(req, res) {
     const id = req.params.id;
     const user = await db.query("select * from users where id = $1", [id]);
     res.json(user.rows[0]);
   }
-  async createUser(req, res) {
+
+  async authorize(req, res) {
+    const { user_name, password } = req.body;
+    const user = await db.query("select * from users where user_name = $1", [user_name]);
+    const isPasswordsEqual = await checkPassword(password, user.rows[0].password);
+    if (!isPasswordsEqual) {
+      res.statusCode = 400;
+      res.end("Access denied!");
+      return;
+    }
+    res.end("Access allowed!");
+  }
+
+   createUser = async (req, res) => {
     const { email, user_name, password, registration_date, permalink } = req.body;
+    const usersInDB = await this.getUsers();
+    const errors = [];
+    usersInDB.forEach(user => {
+      if (user.email === email) errors.push("Email already in use!");
+      if (user.permalink === permalink) errors.push("Permalink already in use!");
+      if (user.user_name === user_name) errors.push("User name already in use!");
+    });
+    if (errors.length) {
+      res.statusCode = 400;
+      res.end(errors.join("\n"))
+      return;
+    }
+    const hashedPassword = await encrypt.hashPassword(password);
     const newUser = await db.query(
       `INSERT INTO users (email, user_name, password, permalink,  registration_date) values ($1, $2, $3, $4, $5) RETURNING *`,
-      [email, user_name, password, permalink, registration_date]
+      [email, user_name, hashedPassword, permalink, registration_date]
     );
     res.json(newUser.rows[0]);
   }
+
   async deleteSingleUser(req, res) {
     const id = req.params.id;
     const user = await db.query("delete from users where id = $1", [id]);
