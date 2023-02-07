e83c8e85d896011f019a6abf4152e30dd381811b
ranynigth <rany4ka@gmail.com>
2023-02-07 20:51:15 +0300

    fix: CORS policy off

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const testRouter = require("./routes/test_routes");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json());
 app.use("/", userRouter);
 app.use("/", testRouter);
