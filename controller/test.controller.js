ae2d4287b3aee11aa105b3d31a176aaf1e5e80c4
ranynigth <rany4ka@gmail.com>
2023-02-07 21:43:12 +0300

    fix:tests response

--- controller/test.controller.js
+++ controller/test.controller.js
@@ -3,28 +3,28 @@ const db = require("../db");
 class TestController {
   async getTestsInfo(req, res) {
     const tests = await db.query(`SELECT * FROM tests`);
-    res.json(tests.rows);
+    res.header("Content-Type", "application/json").status(200).send(JSON.stringify(tests.rows[0]));
   }
 
   async getPlayedTestsInfo(req, res) {
     const id = req.params.test_id;
     const tests = await db.query(`SELECT * FROM 
       users_tests WHERE tests_id = ${id}`);
-    res.json(tests.rows);
+    res.header("Content-Type", "application/json").status(200).send(JSON.stringify(tests.rows[0]));
   }
   async getUserPlayedAllTests(req, res) {
     const id = req.params.id;
     console.log(id);
     const user_tests = await db.query(`SELECT * FROM 
       users_tests WHERE user_id = ${id}`);
-    res.json(user_tests.rows);
+    res.header("Content-Type", "application/json").status(200).send(JSON.stringify(user_tests.rows[0]));
   }
   async getUserPlayedSpecificTest(req, res) {
     const test_id = req.params.test_id;
     const user_id = req.params.id;
     const user_tests = await db.query(`SELECT * FROM users_tests
       WHERE user_id = ${user_id} AND tests_id = ${test_id}`);
-    res.json(user_tests.rows);
+    res.header("Content-Type", "application/json").status(200).send(JSON.stringify(user_tests.rows[0]));
   }
 }
 module.exports = new TestController();
