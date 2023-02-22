f4f9b2bde5dc4cd17ff37f673f0682a55323dcde
ranynigth <rany4ka@gmail.com>
2023-02-22 21:11:01 +0300

    fix:new db url

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { Pool } = require("pg");
 
-const connectionString = "postgresql://postgres:5BJ9TGMbsse1aEs0qAFy@containers-us-west-133.railway.app:7419/railway";
+const connectionString = "postgresql://postgres:JsNdfJEdnuDpIXuyaOpr@containers-us-west-171.railway.app:7274/railway";
 
 const pool = new Pool({
   connectionString,
