e04beca6108b77761c48e57e4bada2a95fa1dc97
AlmondChips <94008966+AlmondChips@users.noreply.github.com>
2023-05-09 19:37:17 +0300

    Update db.js
    test

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { Pool } = require("pg");
 
-const connectionString = "postgresql://postgres:JTd32LlnIL22voammEzE@containers-us-west-133.railway.app:7419/railway";
+const connectionString = "";
 
 const pool = new Pool({
   connectionString,
